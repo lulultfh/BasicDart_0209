a9202198674b3b39cee518a0c1242c5ff05af749
lulultfh <punyalulu05@gmail.com>
2026-02-27 13:26:42 +0700

    feat: menambahkan variable mutable dan immutable

---
+++ Variable/immutable_variable.dart
@@ -0,0 +1,11 @@
+void main() {
+  // // final
+  // final nama = "Arya";
+  // nama = "Lior";
+  // print(nama);
+
+  // const
+  const pi = 9.6678;
+  pi = 9;
+  print(pi);
+}
---
+++ Variable/mutable_variable.dart
@@ -0,0 +1,11 @@
+void main() {
+  // var
+  // var nama = "Arya";
+  // nama = "Lior";
+  // print(nama);
+
+  // dynamic
+  dynamic data = "Hello";
+  data = 9;
+  print(data);
+}
